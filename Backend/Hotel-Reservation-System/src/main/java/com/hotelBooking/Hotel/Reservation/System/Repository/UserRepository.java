c04883a381344089fd2d90426f58c4cc4ee29399
Pramodi <thathsa-ps20190@stu.kln.ac.lk>
2024-12-21 12:28:50 +0530

    create security parts

--- Backend/Hotel-Reservation-System/src/main/java/com/hotelBooking/Hotel/Reservation/System/Repository/UserRepository.java
+++ Backend/Hotel-Reservation-System/src/main/java/com/hotelBooking/Hotel/Reservation/System/Repository/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User,String> {
-    boolean existByEmail(String email);
+    boolean existsByEmail(String email);
 
     Optional<User> findByEmail(String email);
 
