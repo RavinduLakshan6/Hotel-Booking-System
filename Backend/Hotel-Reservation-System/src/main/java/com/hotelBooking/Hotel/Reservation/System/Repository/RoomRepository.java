1155754126e498ddaa34f5d37822585bff11f7e1
Prabhath Manashantha <herathh-ps20019@stu.kln.ac.lk>
2024-12-22 16:46:41 +0530

    created room service section

--- Backend/Hotel-Reservation-System/src/main/java/com/hotelBooking/Hotel/Reservation/System/Repository/RoomRepository.java
+++ Backend/Hotel-Reservation-System/src/main/java/com/hotelBooking/Hotel/Reservation/System/Repository/RoomRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface RoomRepository extends MongoRepository<Room, String> {
@@ -15,6 +16,12 @@ public interface RoomRepository extends MongoRepository<Room, String> {
     @Query("{'bookings':{$size:0}}")
     List<Room> findAvailableRooms();
 
+    @Query("SELECT r FROM Room r WHERE r.roomType LIKE %:roomType% AND r.id NOT IN (SELECT bk.room.id FROM Booking bk WHERE" +
+            "(bk.checkInDate <= :checkOutDate) AND (bk.checkOutDate >= :checkInDate))")
+    List<Room> findAvailableRoomsByDatesAndTypes(LocalDate checkInDate, LocalDate checkOutDate, String roomType);
+
     List<Room> findByRoomTypeLikeAndIdNotIn(String roomType, List<String> ids);
 
+    @Query("SELECT r FROM Room r WHERE r.id NOT IN (SELECT b.room.id FROM Booking b)")
+    List<Room> getAllAvailableRooms();
 }
--- Backend/Hotel-Reservation-System/src/main/java/com/hotelBooking/Hotel/Reservation/System/Service/Interface/RoomService.java
+++ Backend/Hotel-Reservation-System/src/main/java/com/hotelBooking/Hotel/Reservation/System/Service/Interface/RoomService.java
@@ -1,5 +1,27 @@
 package com.hotelBooking.Hotel.Reservation.System.Service.Interface;
 
-public interface RoomService {
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import com.hotelBooking.Hotel.Reservation.System.DTO.Response;
+
+public interface RoomService {
+    Response addNewRoom(MultipartFile photo, String roomType, BigDecimal roomPrice, String description);
+    
+    List<String> getAllRoomTypes();
+
+    Response getAllRooms();
+
+    Response deleteRoom(String roomId);
+
+    Response updateRoom(String roomId, String description, String roomType, BigDecimal roomPrice, MultipartFile photo);
+
+    Response getRoomById(String roomId);
+
+    Response getAvailableRoomsByDataAndType(LocalDate checkInDate, LocalDate checkOutDate, String roomType);
+
+    Response getAllAvailableRooms();
 }
