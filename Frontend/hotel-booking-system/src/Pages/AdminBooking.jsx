8f980a4b70bb5260a9b8db5a2e1f83314f5d7016
Prabhath Manashantha <herathh-ps20019@stu.kln.ac.lk>
2024-12-23 20:13:42 +0530

    admin side created

---
+++ Frontend/hotel-booking-system/src/Pages/AdminBooking.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavAdmin from '../Components/Navigation-Bar/NavAdmin';
+
+export default function adminBooking() {
+  return (
+    <div>
+
+        <NavAdmin/>
+
+    </div>
+  )
+}
