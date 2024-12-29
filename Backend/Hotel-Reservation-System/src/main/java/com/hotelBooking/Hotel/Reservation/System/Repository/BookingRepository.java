843fd5934e6784f227fb671571e9caa804531d31
Prabhath Manashantha <herathh-ps20019@stu.kln.ac.lk>
2024-12-29 18:01:16 +0530

    reporisoty changes

--- Backend/Hotel-Reservation-System/src/main/java/com/hotelBooking/Hotel/Reservation/System/Repository/BookingRepository.java
+++ Backend/Hotel-Reservation-System/src/main/java/com/hotelBooking/Hotel/Reservation/System/Repository/BookingRepository.java
@@ -10,10 +10,10 @@ import java.util.Optional;
 
 public interface BookingRepository extends MongoRepository<Booking, String> {
 
-//    Optional<Booking>findByBookingConfirmationCode(String confirmationCode);
-//
-//    @Query("{ 'checkInDate': { $lte: ?1}, 'checkOutDate': { $gte: ?0} }")
-//    List<Booking>findBookingsByDataRange(LocalDate checkInDate, LocalDate checkOutDate);
-//
+   // Optional<Booking>findByBookingConfirmationCode(String confirmationCode);
+
+   // @Query("{ 'checkInDate': { $lte: ?1}, 'checkOutDate': { $gte: ?0} }")
+   // List<Booking>findBookingsByDataRange(LocalDate checkInDate, LocalDate checkOutDate);
+
 
 }
