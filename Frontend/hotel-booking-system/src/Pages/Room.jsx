c17f3944a18abdc24b1683bf3be3c25ce15e6ebf
Prabhath Manashantha <herathh-ps20019@stu.kln.ac.lk>
2025-01-13 21:24:30 +0530

    few changes of room component

--- Frontend/hotel-booking-system/src/Pages/Room.jsx
+++ Frontend/hotel-booking-system/src/Pages/Room.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import Nav from '../Components/Navigation-Bar/Nav';
-import Hero from '../Components/Hero/Hero';
-import RoomsImg from '../Assets/bg6.jpg';
-import Category from '../Components/Category/Category';
-import Footer from '../Components/Footer/Footer';
+// import React from 'react'
+// import Nav from '../Components/Navigation-Bar/Nav';
+// import Hero from '../Components/Hero/Hero';
+// import RoomsImg from '../Assets/bg6.jpg';
+// import Category from '../Components/Category/Category';
+// import Footer from '../Components/Footer/Footer';
 
-export default function Room() {
-  return (
-    <div>
+// export default function Room() {
+//   return (
+//     <div>
 
-        <Nav/>
-        <Hero
-        cName = "hero-mid"
-        heroImg = {RoomsImg}
-        title = "Rooms"
-        />
+//         <Nav/>
+//         <Hero
+//         cName = "hero-mid"
+//         heroImg = {RoomsImg}
+//         title = "Rooms"
+//         />
 
-        <Category/>
+//         <Category/>
 
-        <Footer/>
+//         <Footer/>
 
-    </div>
-  )
-}
+//     </div>
+//   )
+// }
