5fdb8d31e9f12aef5b983066bf8bc4e7e4b46b80
Prabhath Manashantha <herathh-ps20019@stu.kln.ac.lk>
2025-01-14 20:21:20 +0530

    room details page created

--- Frontend/hotel-booking-system/src/App.js
+++ Frontend/hotel-booking-system/src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter,Routes, Route,Navigate } from 'react-router-dom';
 import HomePage from './Components/Home/HomePage';
 import Navbar from './Components/Navigation-Bar/Navbar';
 import Footer from './Components/Footer/Footer';
+import AllRoomsPage from './Components/Room/AllRoomsPage';
+import RoomDetailsPage from './Components/Room/RoomDetailsPage';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
      <div className='content'>
       <Routes>
         <Route exact path='/home' element={<HomePage/>}/>
+        <Route exact path='/rooms' element={<AllRoomsPage/>}/>
+        <Route path='/room-details-book/roomId' element={<RoomDetailsPage/>}/>
       </Routes>
       </div> 
       <Footer/>
