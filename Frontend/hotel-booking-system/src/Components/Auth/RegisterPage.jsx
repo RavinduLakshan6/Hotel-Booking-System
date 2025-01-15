b6ad579a806e50bcd670165648e1ec23afe3e71b
Ravindu <lakshan-ps20159@stu.kln.ac.lk>
2025-01-15 20:38:05 +0530

    create api service

--- Frontend/hotel-booking-system/src/Components/Auth/RegisterPage.jsx
+++ Frontend/hotel-booking-system/src/Components/Auth/RegisterPage.jsx
@@ -27,6 +27,36 @@ function RegisterPage() {
         return true;
     };
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (!validateForm()) {
+            setErrorMessage('Please fill all the fields.');
+            setTimeout(() => setErrorMessage(''), 5000);
+            return;
+        }
+        try {
+            const response = await ApiService.registerUser(formData);
+
+            if (response.statusCode === 200) {
+                setFormData({
+                    name: '',
+                    email: '',
+                    password: '',
+                    phoneNumber: ''
+                });
+                setSuccessMessage('User registered successfully');
+                setTimeout(() => {
+                    setSuccessMessage('');
+                    navigate('/');
+                }, 3000);
+            }
+        }
+         catch (error) {
+            setErrorMessage(error.response?.data?.message || error.message);
+            setTimeout(() => setErrorMessage(''), 5000);
+        }
+    };
+
 }
 
 export default RegisterPage;
