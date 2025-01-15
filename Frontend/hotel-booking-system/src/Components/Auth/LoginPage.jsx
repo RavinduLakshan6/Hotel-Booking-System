647f6b6855bb5ca885e2f17391843f2480c532cb
Ravindu <lakshan-ps20159@stu.kln.ac.lk>
2025-01-15 21:03:26 +0530

    validation part

--- Frontend/hotel-booking-system/src/Components/Auth/LoginPage.jsx
+++ Frontend/hotel-booking-system/src/Components/Auth/LoginPage.jsx
@@ -1,3 +1,30 @@
 import React, { useState } from "react";
 import { useNavigate,useLocation } from "react-router-dom";
 import ApiService from "../../Service/ApiService";
+
+function LoginPage() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+    const location = useLocation();
+
+  const from = location.state?.from?.pathname || '/home';
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (!email || !password) {
+            setError('Please fill in all fields.');
+            setTimeout(() => setError(''), 5000);
+            return;
+        }
+
+        
+    };
+
+    
+}
+
+export default LoginPage;
