40d62c6b6e4c4750738cb0c19ed7588019dbadf8
Pramodi <thathsa-ps20190@stu.kln.ac.lk>
2025-01-18 21:57:55 +0530

    modify manageroom page

---
+++ Frontend/hotel-booking-system/src/Components/Admin/ManageRoomPage.jsx
@@ -0,0 +1,5 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import ApiService from '../../service/ApiService';
+import Pagination from '../Pagination/Pagination'
+import RoomResult from '../Room/RoomResult';
