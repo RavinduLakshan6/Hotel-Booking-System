63ce280c8539ce84795ab93285e03cbe986a8973
Pramodi <thathsa-ps20190@stu.kln.ac.lk>
2025-01-19 10:16:59 +0530

    modify manage booking

---
+++ Frontend/hotel-booking-system/src/Components/Admin/ManageBookingPage.jsx
@@ -0,0 +1,4 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import ApiService from '../../Components/Service/ApiService';
+import Pagination from '../Components/Pagination/Pagination';
