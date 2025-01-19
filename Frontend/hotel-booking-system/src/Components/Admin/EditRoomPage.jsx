55ac9afb0198aa12516f99963de05ebad90d37fe
Pramodi <thathsa-ps20190@stu.kln.ac.lk>
2025-01-19 14:11:42 +0530

    modify the edit room page

---
+++ Frontend/hotel-booking-system/src/Components/Admin/EditRoomPage.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect} from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import ApiService from "../../Service/ApiService";
+
+const EditRoomPage = () => {
+    const { roomId } = useParams();
+    const navigate = useNavigate();
+    const [roomDetails, setRoomDetails] = useState({
+        roomPhotoUrl: '',
+        roomType: '',
+        roomPrice: '',
+        roomDescription: '',
+    });
+    const [file, setFile] = useState(null);
+    const [preview, setPreview] = useState(null);
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
+
+   
+}
